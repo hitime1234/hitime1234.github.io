afeaeb9bd097e0aeed1b2ea542940c65a4fdec28
Mathea <Fox@yae.gay>
2023-11-04 02:51:23 +0000

    Website Build current host 0.5a
    Update to website

---
+++ javascript/stuff.js
@@ -0,0 +1,20 @@
+function github(){
+    window.location.href =  ("https://github.com/hitime1234");  
+    console.log("Check out my github!");
+}
+
+function PronounPage(){
+    window.location.href =  ("https://pron.yae.gay");  
+    console.log("Check out my pronoun page!");
+}
+
+function akkoma(){
+    window.location.href =  ("https://akkoma.yae.gay");  
+    console.log("Check out my frediverse page!");
+}
+
+
+
+function Accept(){
+    window.location.href = ("https://www-users.york.ac.uk/~nkx509/accept.html")
+}
